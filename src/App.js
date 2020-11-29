7153c951eb6c1cd765dea8ff3cfcb011fa14bb1b
aithaprasad <aitha.prasad@bennett.edu.in>
2020-11-29 15:52:00 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import Table from "./components/Table.js";
 import "./App.css";
 //import Pie from "./Pie.js";
-//import { PieChart } from "react-minimal-pie-chart";
+import { PieChart } from "react-minimal-pie-chart";
 const App = () => {
   return (
     <div className="App">
+      <h1>Covid World Statistics</h1>
+      <div className="FullApp">
+        <PieChart
+          data={[
+            { title: "Deaths", value: 1431955, color: "#CD5C5C" },
+            { title: "Recovered", value: 39062353, color: "#2E8B57" },
+            { title: "Active", value: 60964864, color: "#1E90FF" },
+          ]}
+          radius={30}
+        />
+
+        <div>
+          <h5 className="Red">Deaths 1431955</h5>
+
+          <h5 className="Green">Recovered 39062353</h5>
+
+          <h5 className="Blue">Active 60964864</h5>
+        </div>
+      </div>
       <Table />
     </div>
   );
--- src/Pie.js
+++ src/Pie.js
@@ -6,6 +6,7 @@ import { PieChart } from "react-minimal-pie-chart";
     { title: "Two", value: 15, color: "#C13C37" },
     { title: "Three", value: 20, color: "#6A2135" },
   ]}
+  radius={15}
 />;
 
 export default PieChart;
