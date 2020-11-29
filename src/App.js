9b184f05084fcdbed53fc4d5aa1fe7d3ecb61801
aithaprasad <aitha.prasad@bennett.edu.in>
2020-11-29 12:56:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Table from "./components/Table.js";
+//import Pie from "./Pie.js";
+//import { PieChart } from "react-minimal-pie-chart";
+const App = () => {
+  return (
+    <div className="App">
+      <Table />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,77 @@
+//import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
+import Search from "./Search.js";
+
+const Table = () => {
+  const [data, setData] = useState({});
+  // eslint-disable-next-line no-unused-vars
+  const [hasError, setErrors] = useState(false);
+  const [search, setSearch] = useState("");
+
+  async function fetchData() {
+    const res = await fetch("https://api.covid19api.com/summary");
+    res
+      .json()
+      .then((res) => setData(Array.from(res.Countries)))
+      .catch((err) => setErrors(err));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const filterData = useMemo(() => {
+    let filteredData = data;
+
+    if (search) {
+      console.log(search);
+      filteredData = filteredData.filter((comment) =>
+        comment.Country.toLowerCase().includes(search.toLowerCase())
+      );
+      console.log(filteredData.slice(0, 5));
+    }
+    return Array.from(filteredData);
+  }, [data, search]);
+
+  return (
+    <>
+      <div>
+        <Search
+          onSearch={(value) => {
+            setSearch(value);
+          }}
+        />
+      </div>
+      <table id="myTable">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Confirmed</th>
+            <th>Recovered</th>
+            <th>Deaths</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filterData.length > 0 ? (
+            filterData.map((droplet) => {
+              return (
+                <tr key={droplet.CountryCode}>
+                  <td>{droplet.Country}</td>
+                  <td>{droplet.TotalConfirmed}</td>
+                  <td>{droplet.TotalRecovered}</td>
+                  <td>{droplet.TotalDeaths}</td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td colSpan="5">No Matches Found...</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Table;
